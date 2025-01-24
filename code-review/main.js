7ebe55d83a08b85d4b0e030c6e68d1fafe05c836
Destiny <destinyhardtw@gmail.com>
2025-01-24 13:35:27 -0500

    my first commit

---
+++ code-review/main.js
@@ -0,0 +1,45 @@
+// console.log("Hello World");
+
+// let age = prompt("How old are you");
+
+// if (age > 19 && age < 30) {
+//     console.log("You Are In Your 20s");
+// } else if (age > 29 && age < 40) {
+//     console.log("You are in Your 30s");
+// } else if (age > 39 && age < 50) {
+//     console.log("You are in Your 40s");
+// } else {
+//     console.log("IDK")
+// }
+
+
+// red, blue, yellow
+// let red = prompt ("Are you mixing red paint?");
+// let blue = prompt ("Are you mixing blue paint?");
+// let yellow = prompt ("Are you mixing yellow paint?");
+// console.log(red, yellow, blue)
+
+// if (red == "yes" && blue == "yes" && yellow == "yes") {
+//     console.log("black");
+// } else if (red == "yes" && blue == "yes" && yellow == "no") {
+//     console.log("purple");
+// } else if (red == "yes" && blue == "no" && yellow == "yes") {
+//     console.log("orange");
+// }else if (red == "no" && blue == "yes" && yellow == "no") {
+//     console.log("green");
+// } else if (red == "yes" && blue == "no" && yellow == "no") {
+//     console.log("red");
+// } else if (red == "no" && blue == "yes" && yellow == "no") {
+//     console.log("blue");
+// } else if (red == "no" && blue == "no" && yellow == "yes") {
+//     console.log("yellow");
+// } else {
+//     console.log("white");
+// }
+
+
+let i = 1;
+while (i < 99) {
+    console.log(i);
+    i = i + 2;
+}
