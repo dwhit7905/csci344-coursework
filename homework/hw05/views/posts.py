46510c9ab0df77f44b41ae138e9ec37b0e4dd7af
Destiny <destinyhardtw@gmail.com>
2025-05-02 23:13:49 -0400

    hw5, will continue working on until monday

--- homework/hw05/views/posts.py
+++ homework/hw05/views/posts.py
@@ -5,7 +5,7 @@ from flask_restful import Resource
 
 from models import db
 from models.post import Post
-from views import get_authorized_user_ids
+from views import get_authorized_user_ids, can_view_post
 
 
 def get_path():
@@ -19,9 +19,33 @@ class PostListEndpoint(Resource):
 
     def get(self):
 
+        count = request.args.get("limit")
+        if count is None:
+            count = 20
+        try:
+            count = int(count)
+        except: 
+            return Response(
+                json.dumps({
+                    "message": "limit must be an integer"
+                }),
+                mimetype="applications/json",
+                status=400
+            )
+        print(count)
+
+        if count > 50: 
+            return Response(
+                json.dumps({
+                    "message": "limit must be less than 50"
+                }),
+                mimetype="applications/json",
+                status=400
+            )
+
         # giving you the beginnings of this code (as this one is a little tricky for beginners):
         ids_for_me_and_my_friends = get_authorized_user_ids(self.current_user)
-        posts = Post.query.filter(Post.user_id.in_(ids_for_me_and_my_friends))
+        posts = Post.query.filter(Post.user_id.in_(ids_for_me_and_my_friends)).limit(count)
 
         # TODO: add the ability to handle the "limit" query parameter:
 
@@ -30,7 +54,39 @@ class PostListEndpoint(Resource):
 
     def post(self):
         # TODO: handle POST logic
-        return Response(json.dumps({}), mimetype="application/json", status=201)
+
+        image_url = request.json.get('image_url')
+        caption = request.json.get('caption')
+        alt_text = request.json.get('alt_text')
+
+
+
+        if image_url is None:
+             return Response(
+                 json.dumps({
+                    "message": "image_url is a required field."
+                }),
+                mimetype="applications/json",
+                status=400
+             )
+        
+        print(image_url, caption, alt_text)
+
+        post = Post (
+            image_url=image_url,
+            user_id=self.current_user.id,
+            caption=caption,
+            alt_text=alt_text,
+        )
+
+        db.session.add(post)
+        db.session.commit()
+
+        
+        return Response(
+            json.dumps(post.to_dict(user=self.current_user)),
+            mimetype="application/json", 
+            status=201)
 
 
 class PostDetailEndpoint(Resource):
@@ -39,28 +95,90 @@ class PostDetailEndpoint(Resource):
         self.current_user = current_user
 
     def patch(self, id):
-        print("POST id=", id)
-        # TODO: Add PATCH logic...
-        return Response(json.dumps({}), mimetype="application/json", status=200)
+
+        post = Post.query.get(id)
+        if post is None:
+               return Response(
+                json.dumps({"Message": f"Post id={id} not found"}),
+                mimetype="application/json",
+                status=404,
+        )
+
+        if post.user_id == self.current_user.id:
+                return Response(
+                json.dumps({"Message": f"You are not allowed to modify post id={id}"}),
+                mimetype="application/json",
+                status=403,
+        )
+
+        data = request.json
+        caption = data.get("caption")
+        image_url = data.get("image_url")
+        alt_text = data.get("alt_text")
+
+        if caption is not None:
+            post.caption = caption
+        if image_url is not None:
+            post.image_url = image_url
+        if alt_text is not None:
+            post.alt_text = alt_text
+
+        db.session.commit()
+
+
+        return Response(
+            json.dumps(post.to_dict(user=self.current_user)), 
+            mimetype="application/json", 
+            status=200)
 
     def delete(self, id):
-        print("POST id=", id)
+        post = Post.query.get(id)
+        if post is None:
+               return Response(
+                json.dumps({"Message": f"Post id={id} not found"}),
+                mimetype="application/json",
+                status=404,
+        )
+
+        if post.user_id == self.current_user.id:
+                return Response(
+                json.dumps({"Message": f"You are not allowed to modify post id={id}"}),
+                mimetype="application/json",
+                status=403,
+        )
+
+        Post.query.filter_by(id=id).delete()
+        db.session.commit()
+
 
         # TODO: Add DELETE logic...
         return Response(
-            json.dumps({}),
+            json.dumps({"message": f"Post id={id} has been successfully deleted"}),
             mimetype="application/json",
             status=200,
         )
 
     def get(self, id):
-        print("POST id=", id)
-        # TODO: Add GET logic...
-        return Response(
-            json.dumps({}),
-            mimetype="application/json",
-            status=200,
+        # print("POST id=", id)
+
+        is_authorized_and_exists = can_view_post(id, self.current_user)
+
+        if is_authorized_and_exists:
+            post = Post.query.get(id)
+
+            return Response(
+                json.dumps(post.to_dict(user=self.current_user)),
+                mimetype="application/json",
+                status=200,
         )
+        else:
+            return Response(
+                json.dumps({"Message": f"Post id={id} not found"}),
+                mimetype="application/json",
+                status=404,
+        )
+            
+        
 
 
 def initialize_routes(api, current_user):
--- tutorials/tutorial10/answers.sql
+++ tutorials/tutorial10/answers.sql
@@ -15,41 +15,64 @@ SELECT last_name, first_name FROM users ORDER BY last_name;
 
 
 -- Exercise 4: Filtering
-
+SELECT ID user_id, image_url 
+FROM posts 
+JOIN users ON user_id = id
+WHERE first_name = 'Nicholas';
 
 
 
 -- Exercise 5: Filtering with logical operators
-
-
+SELECT ID user_id, image_url 
+FROM posts 
+JOIN users ON user_id = id
+WHERE user_id = 26 or  user_id = 12;
 
 
 -- Exercise 6: Using functions in a select statement
 
-
+SELECT count(*) 
+FROM posts
+;
 
 
 -- Exercise 7: Aggregating data
-
+SELECT user_id, count(*) 
+FROM comments
+GROUP BY user_id
+ORDER BY count(*) desc
+;
 
 
 
 -- Exercise 8: Joining: two tables
-
+SELECT posts.id, posts.user_id, posts.image_url
+FROM posts
+JOIN users on posts.user_id = users.id
+WHERE users.first_name = 'Nicholas' or users.first_name = 'Rebecca';
 
 
 
 -- Exercise 9: More joining practice: two tables
-
+SELECT posts.id, posts.pub_date, following.following_id
+FROM posts
+JOIN following on posts.user_id = following.following_id
+WHERE following.user_id = 26 ; 
 
 
 
 -- Exercise 10: More joining practice: three tables (Optional)
-
+SELECT posts.id, posts.pub_date, following.following_id
+FROM posts
+JOIN following on posts.user_id = following.following_id
+WHERE following.user_id = 26 ; 
 
 
 
 -- Exercise 11: Inserting records
+SELECT
+FROM bookmarks
+WHERE ;
 
 
 
